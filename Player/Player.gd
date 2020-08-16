86f3b5a1179b8f049d80f862a3c38e66213c013b
calsf <lefrogjones@gmail.com>
2020-08-16 18:39:40 -0400

    adding elevation 2 and fixing elevation/jump system

--- Player/Player.gd
+++ Player/Player.gd
@@ -11,9 +11,13 @@ var has_double_jumped = false
 var has_jumped = false
 var is_tumbling = false
 
-var max_height = 64
+var max_height = 48
 var added_height = 0
 var ground_elevation = 32
+
+# when entered new elevation, waits to hit ground before assigning new ground elevation
+# uses added_height to determine height until no longer awaiting_new_elevation
+var awaiting_new_elevation = false
 
 onready var player_shadow = $Shadow
 onready var player_sprite = $PlayerSprite
@@ -63,13 +67,14 @@ func _physics_process(delta):
 		if added_height > 0:
 			added_height -= 4
 			player_sprite.position.y += 4
-		elif added_height <= ground_elevation:
+		else:
 			is_falling = false
 			max_height = 48
 			added_height = 0
 			has_jumped = false
 			has_double_jumped = false
 			hurtbox.disabled = false
+			awaiting_new_elevation = false
 			
 	else:	
 		# get if player area is overlapping a wall, if it is, tumble down
@@ -82,11 +87,21 @@ func _physics_process(delta):
 			is_tumbling = false
 			collision_shape.disabled = false
 	
+	# calculate current elevation
+	var curr_elevation = added_height
+	if !awaiting_new_elevation:
+		curr_elevation += ground_elevation
+		
 	# boundary mask layer of elevation 1
-	set_collision_mask_bit(3, ground_elevation + added_height < 32)
+	set_collision_mask_bit(3, curr_elevation < 32 and ground_elevation < 32)
+	# boundary mask layer of elevation 2
+	set_collision_mask_bit(7, curr_elevation < 64 and ground_elevation < 64)
 	
 	# wall mask layer of elevation 1
 	set_collision_mask_bit(4, get_collision_mask_bit(3))
+	# wall mask layer of elevation 2
+	set_collision_mask_bit(8, get_collision_mask_bit(7))
+	print(get_collision_mask_bit(8))
 	
 	# Maintain sprite positions when grounded
 	if (added_height <= 0):
@@ -94,10 +109,10 @@ func _physics_process(delta):
 		player_shadow.position.y = 2
 	
 	# PLAYER DEATH CHECK (ELEVATION 0 and not in air)
-	if ground_elevation == 0 and added_height <= 0:
-		position = Vector2.ZERO
-		print("DEAD")
-		ground_elevation = 32
+	#if ground_elevation == 0 and added_height <= 0:
+		#position = Vector2.ZERO
+		#print("DEAD")
+		#ground_elevation = 32
 	
 		
 # input func for tap input events
@@ -121,19 +136,25 @@ func _input(event):
 func _on_DetectElevEntry_area_entered(area):
 	var area_elev = area.get_owner().elevation	# Get elevation of entered area
 	# check for players height before allowing entry into new elevation
-	if (ground_elevation != area_elev and ground_elevation + added_height >= area_elev):
-		ground_elevation = area_elev
-		added_height -= ground_elevation
-		player_sprite.position.y += ground_elevation
-		position.y -= ground_elevation
+	# only enter higher elevations
+	if ground_elevation < area_elev:
+		if (awaiting_new_elevation and added_height >= area_elev or 
+		!awaiting_new_elevation and ground_elevation + added_height >= area_elev):
+			ground_elevation = area_elev
+			added_height -= 32
+			player_sprite.position.y += 32
+			position.y -= 32
+			awaiting_new_elevation = true
 
 # Detect elevation area exit
 func _on_DetectElevArea_area_exited(area):
 	var area_elev = area.get_owner().elevation	# Get elevation of exited area
 	if (ground_elevation == area_elev):
-		added_height += ground_elevation
-		player_sprite.position.y -= ground_elevation
-		position.y += ground_elevation
-		ground_elevation = 0
+		added_height += 32
+		player_sprite.position.y -= 32
+		position.y += 32
+		if ground_elevation == 32:
+			set_collision_mask_bit(3, true)
+		
+		ground_elevation -= 32
 		is_falling = true
-		set_collision_mask_bit(3, true)
