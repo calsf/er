d8b16b53c8d94f6fd1798679e67c69c54037e93e
calsf <lefrogjones@gmail.com>
2020-08-20 10:38:36 -0400

    fix elevation, knockback off wall, and add autotile for tileset1

--- world_objects/player/player.gd
+++ world_objects/player/player.gd
@@ -22,14 +22,10 @@ var is_falling = false
 var has_double_jumped = false
 var has_jumped = false
 var is_tumbling = false		# For falling down a wall
-var jump_height = MAX_HEIGHT	# The number of pixels to jump up
+var jump_height = MAX_HEIGHT	# The number of pixels to jump up (the target added_height will try to reach when is jumping)
 var added_height = 0	# Current number of pixels above ground, grounded at 0
 var elevations = [0, GlobalConst.ELEVATION_UNIT * 1, GlobalConst.ELEVATION_UNIT * 2, GlobalConst.ELEVATION_UNIT * 3]
 var ground_elevation = elevations[1]	# The elevation of height in which player is considered grounded
-var last_ground_elevation = elevations[1]	# The previous ground elevation for when player enters a new elevation but is not yet grounded
-
-# When entered new elevation, need to wait to be grounded before assigning new ground elevation
-var awaiting_new_ground_elevation = false
 
 onready var player_shadow = $Shadow
 onready var player_sprite = $PlayerSprite
@@ -42,19 +38,19 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
-	# Applies falling effect to player if they are caught inside a wall
-	# Provides more accurate drop off from elevations as well
-	if is_tumbling and added_height <= 0:
-		velocity.y += 25
-		velocity = move_and_slide(velocity)
-		check_overlapping_wall()
-		return
-		
 	# Gradually decrease knockback value and knockback strength to 0 and apply current knockback
 	# The knockback and curr_knockback_strength should be equal or decreasing at same rate
 	knockback = knockback.move_toward(Vector2.ZERO, DECEL * delta)
 	curr_knockback_strength = clamp(curr_knockback_strength - (DECEL * delta), 0, 9999)
 	knockback = move_and_slide(knockback)
+	
+	# Applies falling effect to player if they are caught inside a wall
+	# Provides more accurate drop off from elevations as well
+	if is_tumbling and added_height <= 0:
+		velocity.y += MAX_SPEED * delta
+		velocity = move_and_slide(velocity)
+		check_overlapping_wall()
+		return
 	
 	# Only collide with world objects when in knockback state
 	set_collision_mask_bit(0, knockback != Vector2.ZERO)
@@ -117,8 +113,6 @@ func _physics_process(delta):
 			is_falling = false
 			has_jumped = false
 			has_double_jumped = false
-			last_ground_elevation = ground_elevation # When grounded, can set last ground elevation to current ground elevation
-			awaiting_new_ground_elevation = false	# No longer waiting for new ground elevation to be applied because player is grounded
 	else:
 		# When grounded, check if player ends up overlapping with a wall
 		check_overlapping_wall()
@@ -188,7 +182,6 @@ func check_overlapping_wall():
 	# get if player area is overlapping a wall, if it is, tumble down
 	# isTumbling will disable player input
 	if overlapping_wall_area.get_overlapping_bodies().size() > 0:
-		print("OVERLAPPING WALL")
 		collision_shape.disabled = true
 		is_tumbling = true
 	else:
@@ -205,15 +198,8 @@ func death_check():
 		
 func get_curr_elevation():
 	# Calculate current elevation and then set collision masks accordingly
-	var curr_elevation = added_height
-	if !awaiting_new_ground_elevation:
-		# Use current ground elevation is grounded and not waiting for new ground elevation
-		curr_elevation += ground_elevation
-	else:
-		# Use last ground elevation if waiting for player to be grounded to set new ground elevation
-		curr_elevation += last_ground_elevation
+	var curr_elevation = added_height + ground_elevation
 	return curr_elevation
-
 
 # Listen for button press
 func _input(event):
@@ -228,7 +214,7 @@ func _input(event):
 		is_falling = false
 	elif Input.is_action_just_pressed("jump") and has_jumped and !has_double_jumped:
 		has_double_jumped = true
-		jump_height = MAX_HEIGHT * 2
+		jump_height = MAX_HEIGHT + added_height	# Increase target jump height by MAX_HEIGHT
 		is_jumping = true
 		is_falling = false
 		
@@ -238,26 +224,22 @@ func _on_DetectElevEntry_area_entered(area):
 	# Check for player elevation before allowing entry into new elevation
 	# Only enter elevations that are higher than current ground elevation
 	if ground_elevation < area_elev:
-		# When player enters elevation area, they may still be in air and thus awaiting for new ground elevation
-		# Player elevation should use last ground elevation instead of current ground elevation if awaiting new elevation 
-		if (awaiting_new_ground_elevation and last_ground_elevation + added_height >= area_elev or 
-		!awaiting_new_ground_elevation and ground_elevation + added_height >= area_elev):
+		if (ground_elevation + added_height >= area_elev):
 			# Set new ground elevation to the entered elevation
 			ground_elevation = area_elev
 			
-			# Create illusion of entering higher elevation
+			# Create illusion of going up one elevation
+			jump_height -= GlobalConst.ELEVATION_UNIT
 			added_height -= GlobalConst.ELEVATION_UNIT
 			player_sprite.position.y += GlobalConst.ELEVATION_UNIT
 			position.y -= GlobalConst.ELEVATION_UNIT
-			
-			# Entering new elevation must await for new ground elevation which will be set to false when player is grounded
-			awaiting_new_ground_elevation = true
 
 # Detect elevation area exit
 func _on_DetectElevArea_area_exited(area):
 	var area_elev = area.get_owner().elevation	# Get elevation of exited area
 	if (ground_elevation == area_elev):
 		# Create illusion of dropping down one elevation
+		jump_height += GlobalConst.ELEVATION_UNIT
 		added_height += GlobalConst.ELEVATION_UNIT
 		player_sprite.position.y -= GlobalConst.ELEVATION_UNIT
 		position.y += GlobalConst.ELEVATION_UNIT
