bfb1dbb5f495631c1818fd67dd3066983ac7bb86
calsf <lefrogjones@gmail.com>
2020-09-12 21:57:56 -0400

    adding side cata

--- world_objects/catapult/launch_up.gd
+++ world_objects/catapult/launch_up.gd
@@ -1,7 +1,8 @@
 extends Node
 
 export var launch_strength = 0
-onready var player = get_node("../../../YSortWorldObjects/Player")
+export var player_path = "";
+onready var player = get_node(player_path)
 
 func _on_AnimationPlayer_animation_started(anim_name):
 	player.jump_height = launch_strength;
