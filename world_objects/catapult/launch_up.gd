cd6beab0be6d62641726d115720ca196c15fbc65
calsf <lefrogjones@gmail.com>
2020-09-16 21:57:42 -0400

    fin l6, reset jump on launch up

--- world_objects/catapult/launch_up.gd
+++ world_objects/catapult/launch_up.gd
@@ -5,5 +5,7 @@ export var player_path = "";
 onready var player = get_node(player_path)
 
 func _on_AnimationPlayer_animation_started(anim_name):
+	player.has_jumped = false;
+	player.has_double_jumped = false;
 	player.jump_height = launch_strength;
 	player.is_jumping = true;
